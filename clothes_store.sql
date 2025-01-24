3e32aa97923de72741a2293d31e2574d7fbe280d
Andrew <101985948+andrijbikov134@users.noreply.github.com>
2025-01-24 20:26:03 +0200

    Update clothes_store.sql

--- clothes_store.sql
+++ clothes_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Час створення: Січ 18 2025 р., 16:48
+-- Час створення: Січ 24 2025 р., 17:49
 -- Версія сервера: 10.4.32-MariaDB
 -- Версія PHP: 8.2.12
 
@@ -39,7 +39,91 @@ CREATE TABLE `brands` (
 INSERT INTO `brands` (`id`, `title`) VALUES
 (1, 'Zara'),
 (2, 'Columbia'),
-(3, 'Puma');
+(3, 'Puma'),
+(4, 'Adidas'),
+(5, 'Colin\'s'),
+(6, 'H&M'),
+(7, 'Без бренду'),
+(8, 'Nike'),
+(9, 'Other');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='category';
+
+--
+-- Дамп даних таблиці `category`
+--
+
+INSERT INTO `category` (`id`, `title`) VALUES
+(1, 'Women'),
+(2, 'Men'),
+(3, 'Сhildren');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `categorysub`
+--
+
+CREATE TABLE `categorysub` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='categorysub';
+
+--
+-- Дамп даних таблиці `categorysub`
+--
+
+INSERT INTO `categorysub` (`id`, `title`) VALUES
+(1, 'clothes'),
+(3, 'accessories'),
+(4, 'shoes');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `categorysubsub`
+--
+
+CREATE TABLE `categorysubsub` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `pictures_path` varchar(200) NOT NULL,
+  `categorysub_id` int(11) UNSIGNED NOT NULL,
+  `title_ua` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='categorysubsub';
+
+--
+-- Дамп даних таблиці `categorysubsub`
+--
+
+INSERT INTO `categorysubsub` (`id`, `title`, `pictures_path`, `categorysub_id`, `title_ua`) VALUES
+(1, 'jackets_women', '../img/women/clothes/jackets_women.png', 1, 'Куртки'),
+(2, 'jackets_men', '../img/men/clothes/jackets_men.png', 1, 'Куртки'),
+(3, 'jackets_children', '../img/children/clothes/jackets_children.png', 1, 'Куртки'),
+(4, 'pants', '', 1, 'Куртки'),
+(5, 'tshirts', '', 1, 'Куртки'),
+(6, 'thermal_whiteness', '', 1, 'Куртки'),
+(7, 'jerseys', '', 1, 'Куртки'),
+(8, 'shirts', '', 1, 'Куртки'),
+(9, 'sneakers', '', 1, 'Куртки'),
+(10, 'boots', '', 1, 'Куртки'),
+(11, 'kedy', '', 1, 'Куртки'),
+(12, 'flip_flops', '', 1, 'Куртки'),
+(13, 'headwear', '', 1, 'Куртки'),
+(14, 'backpacks', '', 1, 'Куртки'),
+(15, 'scarves', '', 1, 'Куртки'),
+(19, 'gloves', '', 1, 'Куртки'),
+(20, 'down_jackets', '', 1, 'Куртки'),
+(21, 'sweatshirts', '', 1, 'Куртки');
 
 -- --------------------------------------------------------
 
@@ -58,7 +142,44 @@ CREATE TABLE `colors` (
 
 INSERT INTO `colors` (`id`, `title`) VALUES
 (1, 'Синій'),
-(2, 'Жовтий');
+(2, 'Жовтий'),
+(3, 'Білий'),
+(4, 'Блакитний'),
+(5, 'Зелений'),
+(6, 'Чорний'),
+(7, 'Коричневий'),
+(8, 'Рожевий'),
+(9, 'Помаранчевий'),
+(10, 'Червоний'),
+(11, 'Інші кольори'),
+(12, 'Сірий'),
+(13, 'Фіолетовий'),
+(14, 'Бежевий'),
+(15, 'Мультиколір');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `product_id` int(11) UNSIGNED NOT NULL,
+  `comment` varchar(200) NOT NULL,
+  `advantages` varchar(200) NOT NULL,
+  `disadvantages` varchar(200) NOT NULL,
+  `star_quantity` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='comments';
+
+--
+-- Дамп даних таблиці `comments`
+--
+
+INSERT INTO `comments` (`id`, `product_id`, `comment`, `advantages`, `disadvantages`, `star_quantity`, `user_id`) VALUES
+(8, 1, 'ббббб', '000', 'бюбю', 5, 1),
+(9, 1, 'іва', 'іваі', 'віаів', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -77,7 +198,19 @@ CREATE TABLE `countriesproduct` (
 
 INSERT INTO `countriesproduct` (`id`, `title`) VALUES
 (1, 'Ukraine'),
-(2, 'Switzerland');
+(2, 'Switzerland'),
+(3, 'Italy');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `deliverytype`
+--
+
+CREATE TABLE `deliverytype` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='deliverytype';
 
 -- --------------------------------------------------------
 
@@ -95,8 +228,35 @@ CREATE TABLE `materials` (
 --
 
 INSERT INTO `materials` (`id`, `title`) VALUES
-(1, 'Cotton'),
-(2, 'Wool');
+(1, 'Бавовна'),
+(2, 'Вовна'),
+(3, 'Поліестер');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `client_id` int(11) UNSIGNED NOT NULL,
+  `date_order` datetime NOT NULL,
+  `delivery_type_id` int(11) UNSIGNED NOT NULL,
+  `payment_type_id` int(11) UNSIGNED NOT NULL,
+  `recipient_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='orders';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `paymenttype`
+--
+
+CREATE TABLE `paymenttype` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='paymenttype';
 
 -- --------------------------------------------------------
 
@@ -114,20 +274,71 @@ CREATE TABLE `products` (
   `material_id` int(11) UNSIGNED NOT NULL,
   `country_product_id` int(11) UNSIGNED NOT NULL,
   `part_number` varchar(200) NOT NULL,
-  `pictures_path` varchar(200) NOT NULL
+  `pictures_path` varchar(200) NOT NULL,
+  `comment_id` int(11) UNSIGNED NOT NULL,
+  `category_id` int(11) UNSIGNED NOT NULL,
+  `category_sub_id` int(11) UNSIGNED NOT NULL,
+  `category_sub_sub_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='products';
 
 --
 -- Дамп даних таблиці `products`
 --
 
-INSERT INTO `products` (`id`, `title`, `size_id`, `color_id`, `brand_id`, `price`, `material_id`, `country_product_id`, `part_number`, `pictures_path`) VALUES
-(1, 'Футболка бавовняна жіноча Puma 3 S Baby Tee IU2532 2XS Yellow', 1, 2, 3, 1000.00, 1, 1, '4066757286810', './img/woman/clothes/1.png'),
-(2, 'Пуховик чоловічий Protective Down Parka', 3, 2, 2, 6000.00, 1, 1, '4067981263318', './img/woman/clothes/2.png'),
-(3, 'Зимова куртка Outerwear Boy Kids', 1, 1, 2, 3500.00, 1, 2, '4067981263318', './img/woman/clothes/3.png'),
-(4, 'Футболка бавовняна жіноча 3 S Baby Tee', 1, 1, 1, 1000.00, 1, 2, '4066757286810', './img/woman/clothes/4.png'),
-(5, 'Спортивний костюм чоловічий Champion 218680', 2, 1, 2, 3449.00, 1, 2, '8054112953114', './img/woman/clothes/5.png'),
-(6, 'Худі на флісі для хлопчика Nike K NSW CLUB FLC HDY HBR MC HJ3156-224 137-147 см', 1, 2, 2, 1999.00, 1, 2, '197598790403', './img/woman/clothes/6.png');
+INSERT INTO `products` (`id`, `title`, `size_id`, `color_id`, `brand_id`, `price`, `material_id`, `country_product_id`, `part_number`, `pictures_path`, `comment_id`, `category_id`, `category_sub_id`, `category_sub_sub_id`) VALUES
+(1, 'Футболка бавовняна жіноча Puma 3 S Baby Tee IU2532 2XS Yellow', 1, 2, 3, 1000.00, 1, 1, '4066757286810', '../img/popular_product/1.png', 0, 1, 1, 1),
+(2, 'Пуховик чоловічий Protective Down Parka', 3, 2, 2, 3999.00, 1, 1, '4067981263318', './img/popular_product/2.png', 0, 1, 1, 1),
+(3, 'Зимова куртка Outerwear Boy Kids', 1, 1, 2, 3500.00, 1, 2, '4067981263318', './img/popular_product/3.png', 0, 1, 1, 1),
+(4, 'Футболка бавовняна жіноча 3 S Baby Tee', 1, 1, 1, 1000.00, 1, 2, '4066757286810', './img/popular_product/4.png', 0, 1, 1, 1),
+(5, 'Спортивний костюм чоловічий Champion 218680', 2, 1, 2, 3099.00, 1, 2, '8054112953114', './img/popular_product/5.png', 0, 1, 1, 1),
+(6, 'Худі на флісі для хлопчика Nike', 1, 2, 2, 1999.00, 1, 2, '197598790403', './img/popular_product/6.png', 0, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `recipients`
+--
+
+CREATE TABLE `recipients` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `phone` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='recipients';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `product_id` int(11) UNSIGNED NOT NULL,
+  `grade` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='reviews';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='roles';
+
+--
+-- Дамп даних таблиці `roles`
+--
+
+INSERT INTO `roles` (`id`, `title`) VALUES
+(1, 'Administrator'),
+(2, 'Client'),
+(3, 'Seller');
 
 -- --------------------------------------------------------
 
@@ -145,9 +356,55 @@ CREATE TABLE `sizes` (
 --
 
 INSERT INTO `sizes` (`id`, `title`) VALUES
-(1, '48'),
-(2, '50'),
-(3, '52');
+(1, '44'),
+(2, '46'),
+(3, '48'),
+(4, '50'),
+(5, '52'),
+(6, '54'),
+(7, '56'),
+(8, '58'),
+(9, '60'),
+(10, '62'),
+(11, '64'),
+(12, '66'),
+(13, '68');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `tablepartorders`
+--
+
+CREATE TABLE `tablepartorders` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `order_id` int(11) UNSIGNED NOT NULL,
+  `quantity` int(11) UNSIGNED NOT NULL,
+  `product_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='tablepartorders';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `phone` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='users';
+
+--
+-- Дамп даних таблиці `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `phone`, `email`, `password`, `role_id`) VALUES
+(1, 'Client 1', 'Client 1', '+41-025-234-32-23', 'asdfsa@sda.ch', '1', 2);
 
 --
 -- Індекси збережених таблиць
@@ -160,10 +417,36 @@ ALTER TABLE `brands`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Індекси таблиці `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `categorysub`
+--
+ALTER TABLE `categorysub`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `categorysubsub`
+--
+ALTER TABLE `categorysubsub`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categorysub_id` (`categorysub_id`);
+
+--
 -- Індекси таблиці `colors`
 --
 ALTER TABLE `colors`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Індекси таблиці `countriesproduct`
@@ -172,9 +455,31 @@ ALTER TABLE `countriesproduct`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Індекси таблиці `deliverytype`
+--
+ALTER TABLE `deliverytype`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Індекси таблиці `materials`
 --
 ALTER TABLE `materials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `delivery_type_id` (`delivery_type_id`),
+  ADD KEY `paymment_type_id` (`payment_type_id`),
+  ADD KEY `recipient_id` (`recipient_id`);
+
+--
+-- Індекси таблиці `paymenttype`
+--
+ALTER TABLE `paymenttype`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -186,13 +491,51 @@ ALTER TABLE `products`
   ADD KEY `color_id` (`color_id`),
   ADD KEY `brand_id` (`brand_id`),
   ADD KEY `material_id` (`material_id`),
-  ADD KEY `country_product_id` (`country_product_id`);
+  ADD KEY `country_product_id` (`country_product_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `category_sub_id` (`category_sub_id`),
+  ADD KEY `category_sub_sub_id` (`category_sub_sub_id`);
+
+--
+-- Індекси таблиці `recipients`
+--
+ALTER TABLE `recipients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reviews_ibfk_1` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Індекси таблиці `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Індекси таблиці `sizes`
 --
 ALTER TABLE `sizes`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `tablepartorders`
+--
+ALTER TABLE `tablepartorders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Індекси таблиці `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`);
 
 --
 -- AUTO_INCREMENT для збережених таблиць
@@ -202,25 +545,67 @@ ALTER TABLE `sizes`
 -- AUTO_INCREMENT для таблиці `brands`
 --
 ALTER TABLE `brands`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT для таблиці `category`
+--
+ALTER TABLE `category`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблиці `categorysub`
+--
+ALTER TABLE `categorysub`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблиці `categorysubsub`
+--
+ALTER TABLE `categorysubsub`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT для таблиці `colors`
 --
 ALTER TABLE `colors`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT для таблиці `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблиці `countriesproduct`
 --
 ALTER TABLE `countriesproduct`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблиці `deliverytype`
+--
+ALTER TABLE `deliverytype`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблиці `materials`
 --
 ALTER TABLE `materials`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблиці `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблиці `paymenttype`
+--
+ALTER TABLE `paymenttype`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблиці `products`
@@ -229,14 +614,65 @@ ALTER TABLE `products`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT для таблиці `recipients`
+--
+ALTER TABLE `recipients`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблиці `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблиці `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT для таблиці `sizes`
 --
 ALTER TABLE `sizes`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT для таблиці `tablepartorders`
+--
+ALTER TABLE `tablepartorders`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблиці `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Обмеження зовнішнього ключа збережених таблиць
 --
+
+--
+-- Обмеження зовнішнього ключа таблиці `categorysubsub`
+--
+ALTER TABLE `categorysubsub`
+  ADD CONSTRAINT `categorysubsub_ibfk_1` FOREIGN KEY (`categorysub_id`) REFERENCES `categorysub` (`id`);
+
+--
+-- Обмеження зовнішнього ключа таблиці `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Обмеження зовнішнього ключа таблиці `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`delivery_type_id`) REFERENCES `deliverytype` (`id`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`payment_type_id`) REFERENCES `paymenttype` (`id`),
+  ADD CONSTRAINT `orders_ibfk_4` FOREIGN KEY (`recipient_id`) REFERENCES `recipients` (`id`);
 
 --
 -- Обмеження зовнішнього ключа таблиці `products`
@@ -246,7 +682,30 @@ ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`color_id`) REFERENCES `colors` (`id`),
   ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
   ADD CONSTRAINT `products_ibfk_4` FOREIGN KEY (`material_id`) REFERENCES `materials` (`id`),
-  ADD CONSTRAINT `products_ibfk_5` FOREIGN KEY (`country_product_id`) REFERENCES `countriesproduct` (`id`);
+  ADD CONSTRAINT `products_ibfk_5` FOREIGN KEY (`country_product_id`) REFERENCES `countriesproduct` (`id`),
+  ADD CONSTRAINT `products_ibfk_6` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `products_ibfk_7` FOREIGN KEY (`category_sub_id`) REFERENCES `categorysub` (`id`),
+  ADD CONSTRAINT `products_ibfk_8` FOREIGN KEY (`category_sub_sub_id`) REFERENCES `categorysubsub` (`id`);
+
+--
+-- Обмеження зовнішнього ключа таблиці `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Обмеження зовнішнього ключа таблиці `tablepartorders`
+--
+ALTER TABLE `tablepartorders`
+  ADD CONSTRAINT `tablepartorders_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `tablepartorders_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Обмеження зовнішнього ключа таблиці `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
